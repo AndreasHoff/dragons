ed3bc78dc8a18af3a727b7d80e451ed2627bd1c6
AndreasHoff <ah@asseco.dk>
2022-11-04 22:41:26 +0100

    test data added to database with mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,8 @@
 const { dragons } = require('../sampleData.js');
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql');
+
+const Dragons = require('../models/Dragon');
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList, GraphQLNonNull } = require('graphql');
+const Dragon = require('../models/Dragon');
 
 // Dragons Type
 const DragonType = new GraphQLObjectType({
@@ -7,8 +10,8 @@ const DragonType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        wood: { type: GraphQLString },
         fish: { type: GraphQLString },
+        wood: { type: GraphQLString },
         iron: { type: GraphQLString },
         gatheringTime: { type: GraphQLString },
     })
@@ -20,19 +23,49 @@ const RootQuery = new GraphQLObjectType({
         dragons: {
             type: GraphQLList(DragonType),
             resolve(parent, args) {
-                return dragons;
+                return Dragon.find();
             },
         },
         dragon: { 
             type: DragonType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return dragons.find(dragon => dragon.id === args.id);
+                return Dragons.findById(args.id);
             }
         },
     }
 });
 
+// Mutations
+
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addDragon: {
+            type: DragonType,
+            args: { 
+                name: { type: GraphQLNonNull(GraphQLString) },
+                fish: { type: GraphQLNonNull(GraphQLString) },
+                wood: { type: GraphQLNonNull(GraphQLString) },
+                iron: { type: GraphQLNonNull(GraphQLString) },
+                gatheringTime: { type: GraphQLNonNull(GraphQLString) }
+            },
+            resolve(parent, args) {
+                const dragon = new Dragon({
+                    name: args.name,
+                    fish: args.fish,
+                    wood: args.wood,
+                    iron: args.iron,
+                    gatheringTime: args.gatheringTime,
+                });
+
+                return dragon.save();
+            }
+        }
+    }     
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation
 })
